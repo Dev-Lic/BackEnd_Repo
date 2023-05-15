439b498262ee635ab96af144abb26417734da231
Dev-Lic <wijdaneaassouss@gmail.com>
2023-05-15 22:33:39 +0100

    POST method with Null value being saved

--- BackEnd/API/controllers/eventController.js
+++ BackEnd/API/controllers/eventController.js
@@ -56,47 +56,68 @@ const getInvoicebyId = (req, res) => {
 
 //--------------------Post methode to add an invoice--------------------
  async function  addInvoice (req, res) {
-//   const data ={ Billing_Org, Billing_Dept, Charged_Org, Charged_Org_Name, Charged_Dep, Fiscal_Month, Charge_Type, Charge_Unit, Charge_Description="Hey this is me ", Charge_Amount, Billable_Amount, Hyperion_Profit_Center, SAP_Profit_Center, Charge_Category, Revenue_Type, Charged_entity, Year, Month } = req.body;
-   
-//    pool.connect().then(() =>{ 
-//      var request = new sql.Request(pool);
-//       request()
-//       .input("Billing_Org", sql.VarChar(50), Billing_Org)
-//       .input("Billing_Dept", sql.VarChar(50), Billing_Dept)
-//       .input("Charged_Org", sql.VarChar(50), Charged_Org)
-//       .input("Charged_Org_Name", sql.VarChar(50), Charged_Org_Name)
-//       .input("Charged_Dep", sql.VarChar(50), Charged_Dep)
-//       .input("Fiscal_Month", sql.VarChar(50), Fiscal_Month)
-//       .input("Charge_Type", sql.VarChar(50), Charge_Type)
-//       .input("Charge_Unit", sql.VarChar(50), Charge_Unit)
-//       .input("Charge_Description", sql.VarChar(200), Charge_Description)
-//       .input("Charge_Amount", sql.Float(24), Charge_Amount)
-//       .input("Billable_Amount", sql.VarChar(50), Billable_Amount)
-//       .input("Hyperion_Profit_Center", sql.VarChar(50), Hyperion_Profit_Center)
-//       .input("SAP_Profit_Center", sql.VarChar(50), SAP_Profit_Center)
-//       .input("Charge_Category", sql.VarChar(50), Charge_Category)
-//       .input("Revenue_Type", sql.VarChar(50), Revenue_Type)
-//       .input("Charged_entity", sql.VarChar(50), Charged_entity)
-//       .input("Year", sql.Int, Year)
-//       .input("Month", sql.VarChar(50), Month)
-//       .query("INSERT INTO TEIS SET ? " , data ,(err,result) =>{
-//          if(err){
-//             res.send('error') ;
-//          }
-//          else {
-//             send(result);}
-//       })
-//       .then((result) => {
-//        console.log('Hiii')
-//        console.log(result);
-//       }).catch((err) => {
-//          console.error(err);
-//          res.status(500).send("Error fetching users OR database");
-//          });
-//        }).catch((err) => {
-//           console.error(err);
-//           res.status(500).send("Error connecting to database");
-//        });
+
+const Billing_Org = req.body.Billing_Org;
+const Billing_Dept = req.body.Billing_Dept;
+const Charged_Org = req.body.Charged_Org;
+const Charged_Org_Name = req.body.Charged_Org_Name;
+const Charged_Dep = req.body.Charged_Dep;
+const Fiscal_Month = req.body.Fiscal_Month;
+const Charge_Type = req.body.Charge_Type;
+const Charge_Type_Description = req.body.Charge_Type_Description;
+const Charge_Unit = req.body.Charge_Unit;
+const Charge_Description = req.body.Charge_Description || 'Default Description';
+const Charge_Amount = req.body.Charge_Amount;
+const Billable_Amount = req.body.Billable_Amount;
+const Hyperion_Profit_Center = req.body.Hyperion_Profit_Center;
+const SAP_Profit_Center = req.body.SAP_Profit_Center;
+const Charge_Category = req.body.Charge_Category;
+const Revenue_Type = req.body.Revenue_Type;
+const Charged_entity = req.body.Charged_entity;
+const Year = req.body.Year;
+const Month = req.body.Month;
+
+// const query = "INSERT INTO TEIS (Billing_Org, Billing_Dept, Charged_Org, Charged_Org_Name, Charged_Dep, Fiscal_Month, Charge_Type, Charge_Type_Description, Charge_Unit, Charge_Description, Charge_Amount, Billable_Amount, Hyperion_Profit_Center, SAP_Profit_Center, Charge_Category, Revenue_Type, Charged_entity, Year, Month) VALUES ('" + Billing_Org + "', '" + Billing_Dept + "', '" + Charged_Org + "', '" + Charged_Org_Name + "', '" + Charged_Dep + "', '" + Fiscal_Month + "', '" + Charge_Type + "', '" + Charge_Type_Description + "', '" + Charge_Unit + "', '" + Charge_Description + "', '" + Charge_Amount + "', '" + Billable_Amount + "', '" + Hyperion_Profit_Center + "', '" + SAP_Profit_Center + "', '" + Charge_Category + "', '" + Revenue_Type + "', '" + Charged_entity + "', '" + Year + "', '" + Month + "')";
+
+const query = "INSERT INTO [dbo].[TEIS]([Billing_Org],[Billing_Dept],[Charged_Org],[Charged_Org_Name],[Charged_Dep],[Fiscal_Month],[Charge_Type],[Charge_Type_Description],[Charge_Unit],[Charge_Description],[Charge_Amount],[Billable_Amount],[Hyperion_Profit_Center],[SAP_Profit_Center],[Charge_Category],[Revenue_Type],[Charged_entity],[Year],[Month])VALUES( @Billing_Org, @Billing_Dept, @Charged_Org, @Charged_Org_Name, @Charged_Dep, @Fiscal_Month, @Charge_Type, @Charge_Type_Description,@Charge_Unit,@Charge_Description,@Charge_Amount,@Billable_Amount,@Hyperion_Profit_Center,@SAP_Profit_Center,@Charge_Category,@Revenue_Type,@Charged_entity,@Year,@Month) "
+
+   pool.connect().then(() =>{ 
+     const request = new sql.Request(pool);
+      request
+      .input("Billing_Org", sql.NChar(10), Billing_Org)
+      .input("Billing_Dept", sql.NChar(10), Billing_Dept)
+      .input("Charged_Org", sql.NChar(10), Charged_Org)
+      .input("Charged_Org_Name", sql.NChar(60), Charged_Org_Name)
+      .input("Charged_Dep", sql.NChar(10), Charged_Dep)
+      .input("Fiscal_Month", sql.NChar(20), Fiscal_Month)
+      .input("Charge_Type", sql.NChar(50), Charge_Type)
+      .input("Charge_Type_Description", sql.NChar(80),Charge_Type_Description)
+      .input("Charge_Unit", sql.NChar(60), Charge_Unit)
+      .input("Charge_Description", sql.NChar(110), Charge_Description)
+      .input("Charge_Amount", sql.Float, Charge_Amount)
+      .input("Billable_Amount", sql.NChar(10), Billable_Amount)
+      .input("Hyperion_Profit_Center", sql.NChar(10), Hyperion_Profit_Center)
+      .input("SAP_Profit_Center", sql.NChar(10), SAP_Profit_Center)
+      .input("Charge_Category", sql.NChar(10), Charge_Category)
+      .input("Revenue_Type", sql.NChar(10), Revenue_Type)
+      .input("Charged_entity", sql.NChar(10), Charged_entity)
+      .input("Year", sql.Int, Year)
+      .input("Month", sql.NChar(10), Month)
+      .query(query)
+      .then((result) => {
+        res.status(200).send("Invoice saved successfully.");
+      })
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(500)
+          .send("Error while saving data. Please try again later.");
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send("Error while connecting to the database.");
+  });
       };
  
  //--------------------Post methode to add an invoice Ends Here--------------------
